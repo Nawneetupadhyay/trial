5b0326821231ebf3b13dfb61793d1bc8e7c19e3b
Nawneetupadhyay <54641090+Nawneetupadhyay@users.noreply.github.com>
2022-05-05 15:15:36 +0530

    Add files via upload

---
+++ masterdetailfragment2/app/src/main/java/com/example/master_detail_fragment2/placeholder/PlaceholderContent.kt
@@ -0,0 +1,61 @@
+package com.example.master_detail_fragment2.placeholder
+
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ *
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+object PlaceholderContent {
+
+    /**
+     * An array of sample (placeholder) items.
+     */
+    val ITEMS: MutableList<PlaceholderItem> = ArrayList()
+
+    /**
+     * A map of sample (placeholder) items, by ID.
+     */
+    val ITEM_MAP: MutableMap<String, PlaceholderItem> = HashMap()
+
+
+
+    init {
+        // Add some sample items.
+        addItem(PlaceholderItem ("1","apple","apple.jpg"))
+        addItem(PlaceholderItem ("2","apple","mango.jpg"))
+
+        addItem(PlaceholderItem ("3","apple","banana.jpg"))
+        addItem(PlaceholderItem ("4","apple","grapes,jpg"))
+
+    }
+
+    private fun addItem(item: PlaceholderItem) {
+        ITEMS.add(item)
+        ITEM_MAP.put(item.id, item)
+    }
+
+    private fun createPlaceholderItem(position: Int): PlaceholderItem {
+        return PlaceholderItem(position.toString(), "Item " + position, makeDetails(position))
+    }
+
+    private fun makeDetails(position: Int): String {
+        val builder = StringBuilder()
+        builder.append("Details about Item: ").append(position)
+        for (i in 0..position - 1) {
+            builder.append("\nMore details information here.")
+        }
+        return builder.toString()
+    }
+
+    /**
+     * A placeholder item representing a piece of content.
+     */
+    data class PlaceholderItem(val id: String, val fruitname: String, val fruitimage: String) {
+        override fun toString(): String = fruitname
+
+    }
+}
